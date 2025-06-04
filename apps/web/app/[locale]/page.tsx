8ecfe310e6b1e4859b93cc2f725b41cdec216272
Trafim Gest <gest.trafim@gmail.com>
2025-06-04 15:33:31 +0300

    feat: add dayjs usage

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,13 +1,14 @@
 import { useTranslations } from 'next-intl';
 import { ReactElement } from 'react';
-import { mockDateInputs, testDateFnsUsage } from '@date-libs-rnd/shared/utils/date-libs';
+import { mockDateInputs, testDateFnsUsage, testDayjsUsage } from '@date-libs-rnd/shared/utils/date-libs';
 
 export default function Index(): ReactElement {
   const t = useTranslations('web-shared.HOME_PAGE');
 
   //testLuxonUsage(mockDateInputs);
   //testTemporalUsage(mockDateInputs);
-  testDateFnsUsage(mockDateInputs);
+  //testDateFnsUsage(mockDateInputs);
+  testDayjsUsage(mockDateInputs);
 
   return <div>{t('HOME_PAGE_TEXT')}</div>;
 }
